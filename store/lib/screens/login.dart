083783a5f738635941ab2bd46420afe9d5ec97c5
SUNDARA VEL <sundaravelit@gmail.com>
2021-12-27 19:59:39 +0530

    login & logout logic-store app

--- store/lib/screens/login.dart
+++ store/lib/screens/login.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:store/screens/tabs.dart';
@@ -5,61 +6,93 @@ import 'package:store/screens/tabs.dart';
 class LoginScreen extends StatelessWidget {
   LoginScreen({Key? key}) : super(key: key);
 
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  TextEditingController _emailCtrl = TextEditingController();
+  TextEditingController _passwordCtrl = TextEditingController();
+
+  login() {
+    var emailText = (_emailCtrl.text).trim().toLowerCase();
+    var passwordText = _passwordCtrl.text;
+    _auth
+        .signInWithEmailAndPassword(email: emailText, password: passwordText)
+        .then((res) {
+      print(res);
+      Get.offAll(TabsScreen());
+    }).catchError((e) {
+      print(e);
+      Get.showSnackbar(
+        GetBar(
+          message: "${e.toString()}",
+          duration: Duration(
+            seconds: 2,
+          ),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          padding: EdgeInsets.all(32.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                "assets/images/logo.png",
-                height: 120,
-                width: 120,
-              ),
-              SizedBox(height: 16),
-              Text(
-                "Sundar's shop",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 32),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: InputBorder.none,
-                  labelText: "Email Address",
+    return SingleChildScrollView(
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        appBar: AppBar(
+          title: Text("Login"),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/logo.png",
+                  height: 120,
+                  width: 120,
                 ),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: InputBorder.none,
-                  labelText: "Password",
+                SizedBox(height: 16),
+                Text(
+                  "Sundar's shop",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-              ),
-              SizedBox(height: 16),
-              Container(
-                width: double.infinity,
-                height: 48,
-                child: ElevatedButton(
-                  child: Text("LOGIN"),
-                  onPressed: () {
-                    Get.offAll(TabsScreen());
-                  },
+                SizedBox(height: 32),
+                TextField(
+                  controller: _emailCtrl,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: InputBorder.none,
+                    labelText: "Email Address",
+                  ),
                 ),
-              ),
-            ],
+                SizedBox(height: 16),
+                TextField(
+                  controller: _passwordCtrl,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: InputBorder.none,
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(height: 16),
+                Container(
+                  width: double.infinity,
+                  height: 48,
+                  child: ElevatedButton(
+                    child: Text("LOGIN"),
+                    onPressed: () {
+                      login();
+                      // Get.offAll(TabsScreen());
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- store/lib/screens/profile.dart
+++ store/lib/screens/profile.dart
@@ -1,90 +1,103 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:store/screens/login.dart';
 
 class ProfileScreen extends StatelessWidget {
   ProfileScreen({Key? key}) : super(key: key);
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  logout() {
+    _auth.signOut().then((res) {
+      Get.offAll(LoginScreen());
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Edit Store"),
       ),
-      body: Container(
-        padding: EdgeInsets.all(32.0),
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage("assets/images/profile.png"),
-              radius: 60,
-            ),
-            SizedBox(height: 40),
-            TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: InputBorder.none,
-                labelText: "Store Name",
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(32.0),
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage("assets/images/profile.png"),
+                radius: 60,
               ),
-            ),
-            SizedBox(height: 12),
-            TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: InputBorder.none,
-                labelText: "Email Address",
-              ),
-            ),
-            SizedBox(height: 12),
-            TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: InputBorder.none,
-                labelText: "Mobile Number",
-              ),
-            ),
-            SizedBox(height: 12),
-            TextField(
-              maxLines: 4,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: InputBorder.none,
-                labelText: "Store Address",
-              ),
-            ),
-            SizedBox(height: 12),
-            Container(
-              width: double.infinity,
-              height: 48,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  elevation: 0,
-                  primary: Colors.green,
+              SizedBox(height: 40),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Store Name",
                 ),
-                child: Text(
-                  "Save Changes",
-                  style: TextStyle(
-                    fontSize: 16,
+              ),
+              SizedBox(height: 12),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Email Address",
+                ),
+              ),
+              SizedBox(height: 12),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Mobile Number",
+                ),
+              ),
+              SizedBox(height: 12),
+              TextField(
+                maxLines: 4,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none,
+                  labelText: "Store Address",
+                ),
+              ),
+              SizedBox(height: 12),
+              Container(
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    primary: Colors.green,
                   ),
+                  child: Text(
+                    "Save Changes",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  onPressed: () {
+                    Get.back();
+                  },
                 ),
-                onPressed: () {
-                  Get.back();
-                },
               ),
-            ),
-            SizedBox(height: 20),
-            TextButton(
-              onPressed: () {
-                Get.offAll(LoginScreen());
-              },
-              child: Text("Logout"),
-            )
-          ],
+              SizedBox(height: 20),
+              TextButton(
+                onPressed: () {
+                  Get.offAll(LoginScreen());
+                },
+                child: Text("Logout"),
+              )
+            ],
+          ),
         ),
       ),
     );
