e4d80280c62824cdba0722467b8f9cc426bd9acd
SUNDARA VEL <sundaravelit@gmail.com>
2021-12-28 12:29:39 +0530

    read store data from firestore

--- store/lib/screens/profile.dart
+++ store/lib/screens/profile.dart
@@ -1,11 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:store/screens/login.dart';
 
-class ProfileScreen extends StatelessWidget {
-  ProfileScreen({Key? key}) : super(key: key);
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
   FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  TextEditingController _nameCtrl = TextEditingController();
+  TextEditingController _emailCtrl = TextEditingController();
+  TextEditingController _mobileCtrl = TextEditingController();
+  TextEditingController _addressCtrl = TextEditingController();
 
   logout() {
     _auth.signOut().then((res) {
@@ -13,6 +26,27 @@ class ProfileScreen extends StatelessWidget {
     }).catchError((e) {
       print(e);
     });
+  }
+
+  readStoreDetail() {
+    _db.collection("settings").doc("store").get().then((res) {
+      print(res);
+      print(res.data());
+      // we need to add ! to tell value is not null
+      setState(() {
+        _nameCtrl.text = res.data()!["name"];
+        _emailCtrl.text = res.data()!["email"];
+        _mobileCtrl.text = res.data()!["mobile"];
+        _addressCtrl.text = res.data()!["address"];
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    readStoreDetail();
   }
 
   @override
@@ -33,6 +67,7 @@ class ProfileScreen extends StatelessWidget {
               ),
               SizedBox(height: 40),
               TextField(
+                controller: _nameCtrl,
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Colors.grey[200],
@@ -42,6 +77,7 @@ class ProfileScreen extends StatelessWidget {
               ),
               SizedBox(height: 12),
               TextField(
+                controller: _emailCtrl,
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Colors.grey[200],
@@ -51,6 +87,7 @@ class ProfileScreen extends StatelessWidget {
               ),
               SizedBox(height: 12),
               TextField(
+                controller: _mobileCtrl,
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Colors.grey[200],
@@ -60,6 +97,7 @@ class ProfileScreen extends StatelessWidget {
               ),
               SizedBox(height: 12),
               TextField(
+                controller: _addressCtrl,
                 maxLines: 4,
                 decoration: InputDecoration(
                   filled: true,
