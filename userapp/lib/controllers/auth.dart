550fe3affdd3919cc8079dd8988e2a78f65f2759
SUNDARA VEL <sundaravelit@gmail.com>
2021-12-29 11:37:51 +0530

    user app setup (copy from store)

---
+++ userapp/lib/controllers/auth.dart
@@ -0,0 +1,59 @@
+import 'package:get/get.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:userapp/controllers/profile.dart';
+import 'package:userapp/screens/login.dart';
+import 'package:userapp/screens/tabs.dart';
+
+class AuthController extends GetxController {
+  ProfileController _profileCtrl = Get.put(ProfileController());
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  var isUserLoggedIn = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    validate();
+  }
+
+  validate() {
+    _auth.authStateChanges().listen((user) {
+      print(user);
+      if (user != null) {
+        isUserLoggedIn.value = true;
+        _profileCtrl.readStoreDetail();
+      } else {
+        isUserLoggedIn.value = false;
+      }
+    });
+  }
+
+  login(email, password) {
+    email = (email).trim().toLowerCase();
+
+    _auth
+        .signInWithEmailAndPassword(email: email, password: password)
+        .then((res) {
+      print(res);
+      Get.offAll(TabScreen());
+    }).catchError((e) {
+      print(e);
+      Get.showSnackbar(
+        GetBar(
+          message: "${e.toString()}",
+          duration: Duration(
+            seconds: 2,
+          ),
+        ),
+      );
+    });
+  }
+
+  logout() {
+    _auth.signOut().then((res) {
+      Get.offAll(LoginScreen());
+    }).catchError((e) {
+      print(e);
+    });
+  }
+}
